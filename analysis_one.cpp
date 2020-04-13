b7beda3d44f989153960d296420e2748d6bb40a0
Andrea Mancuso <48994231+andreamancuso96@users.noreply.github.com>
2020-04-13 17:56:22 +0200

    Create analysis_one.cpp

---
+++ analysis_one.cpp
@@ -0,0 +1,9 @@
+// Root Analysis for one PMT. 
+// Created by Andrea Mancuso - Alma Mater Studiorum Università di Bologna //
+// 13-04-2020 
+
+
+void analysis_one(){
+
+  cout<<"Hello World \n";
+}
